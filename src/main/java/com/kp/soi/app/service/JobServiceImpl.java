5b8b0bdae7e02d2579161885dff5df1f6d41466f
Han Thu <hmt4224@g.rit.edu>
2021-03-24 07:08:52 -0700

    Update JobServiceImpl.java

--- src/main/java/com/kp/soi/app/service/JobServiceImpl.java
+++ src/main/java/com/kp/soi/app/service/JobServiceImpl.java
@@ -6,17 +6,22 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kp.soi.app.db.entity.clarity.SampleTable;
 import com.kp.soi.app.db.entity.demo.DemoTable;
+import com.kp.soi.app.db.entity.demo.DiagnosisTable;
 //import com.kp.soi.app.db.entity.log.LogTable;
 //import com.kp.soi.app.db.entity.log.LogType;
 import com.kp.soi.app.db.repository.clarity.SampleTableJpaRepository;
 import com.kp.soi.app.db.repository.demo.DemoTableJpaRepository;
 //import com.kp.soi.app.db.repository.log.LogTableJpaRepository;
+import com.kp.soi.app.dto.ModelClass;
 
 import java.time.LocalDateTime;
 
@@ -32,6 +37,15 @@ public class JobServiceImpl {
 //	@Autowired
 //	LogTableJpaRepository logTableJpaRepository;
 	
+	
+	@Autowired
+	EntityManagerFactory emf;
+	public List<ModelClass> fetchFromSource() {
+
+	    EntityManager em = emf.createEntityManager();
+	    javax.persistence.Query query = em.createQuery()
+	}
+	
 	public List<SampleTable> findAllSampleTableList() {
 		return sampleTableJpaRepository.findAll();
 	}
@@ -39,6 +53,11 @@ public class JobServiceImpl {
 	public List<DemoTable> findAllDemoTableList() {
 		return demoTableJpaRepository.findAll();
 	}
+	
+	public List<DiagnosisTable> findAllDxTableList() {
+		return 
+	}
+	
 	
 	public static final String USERNAME= "Test";
 	public static final String FILENAME= "Dashboard";
@@ -62,6 +81,14 @@ public class JobServiceImpl {
 //		saveLog();
 		return l;
 	}
+	
+	
+	public List<DiagnosisTable> upsertdx(List<DiagnosisTable> dxTableList ) {
+		Map<Integer, DiagnosisTable> dxTableMap = dxTableList.stream().collect(Collectors.toMap(a->a.get(), a->a ));
+		
+	
+	
+	
 	
 //	@Transactional(transactionManager = "LogJpaTransactionManager", readOnly = false)
 //	public List<LogTable> saveLog() {
